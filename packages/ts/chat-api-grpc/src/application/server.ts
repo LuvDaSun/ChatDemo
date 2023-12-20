ed3dcf712ef59e7173352329da728a3bd82e342b
Elmer Bulthuis <elmerbulthuis@gmail.com>
2023-12-20 21:43:36 +0100

    grpc server

---
+++ packages/ts/chat-api-grpc/src/application/server.ts
@@ -0,0 +1,5 @@
+import * as common from "chat-api-common";
+
+export class Server {
+  constructor(private readonly context: common.application.Context) {}
+}
---
+++ packages/ts/chat-api-grpc/src/programs/server.ts
@@ -0,0 +1,63 @@
+import * as common from "chat-api-common";
+import http from "http";
+import * as yargs from "yargs";
+import * as application from "../application/index.js";
+
+export function registerServerProgram(argv: yargs.Argv) {
+  return argv.command(
+    "server",
+    "Start chat-api-grpc server",
+    (yargs) =>
+      yargs.option("port", {
+        description: "Port to start the server on",
+        type: "number",
+        default: 50051,
+      }),
+    (argv) => main(argv as MainOptions),
+  );
+}
+
+interface MainOptions {
+  port: number;
+}
+
+async function main(options: MainOptions) {
+  const { port } = options;
+
+  console.log("Starting server...");
+
+  const context = new common.application.Context();
+
+  const server = http.createServer();
+  const applicationServer = new application.Server(context);
+
+  await new Promise<void>((resolve, reject) => server.listen(port, () => resolve()));
+
+  // server.addListener("request", applicationServer.requestHandler);
+
+  console.log("Server started");
+  try {
+    await new Promise<void>((resolve) => {
+      const abort = () => {
+        process.removeListener("SIGINT", abort);
+        process.removeListener("SIGTERM", abort);
+
+        resolve();
+      };
+      process.addListener("SIGINT", abort);
+      process.addListener("SIGTERM", abort);
+    });
+  } finally {
+    console.log("Stopping server...");
+
+    // server.removeListener("request", applicationServer.requestHandler);
+
+    server.closeAllConnections();
+
+    await new Promise<void>((resolve, reject) =>
+      server.close((error) => (error == null ? resolve() : reject(error))),
+    );
+
+    console.log("Server stopped");
+  }
+}
