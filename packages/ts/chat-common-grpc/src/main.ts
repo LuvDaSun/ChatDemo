0487d8ce9e67742f78aa768e535ba73f65a795a6
Elmer Bulthuis <elmerbulthuis@gmail.com>
2023-12-20 21:38:11 +0100

    wip

--- packages/ts/chat-common-grpc/src/main.ts
+++ packages/ts/chat-common-grpc/src/main.ts
@@ -1,8 +1,1 @@
-export * from "./ChatDemo.js";
-export * from "./Message.js";
-export * from "./MessageEvent.js";
-export * from "./MessageNew.js";
-export * from "./MessageSnapshot.js";
-export * from "./Messages.js";
-export * from "./Nothing.js";
-export * from "./service.js";
+export * as types from "./types/index.js";
